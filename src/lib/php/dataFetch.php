fa0951235d01e571a3b4cc0008ff2b874221eb50
Andrew <andrew.ed.roberts@gmail.com>
2017-04-13 11:14:55 -0400

    meh

---
+++ src/lib/php/dataFetch.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+PHP script for inclusion with the Web Map Application prototype. 
+Developed by Andrew Roberts 2017, for Professor X. 
+FirePHP console commands for logging php activity:
+FB::log('Log Message');
+FB::info('Info Message');
+FB::warn('Warn Message');
+FB::error('Error Message');
+FB::trace('Simple Trace');
+
+*/
+
+// Include the FirePHP debugging script
+require('../lib/fb/FirePHPCore/fb.php');
+
+FB::log('script fires');
+
+
+?>
